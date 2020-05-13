d54d3e6d83782647cf9765daa0aea2eaf995357f
jhester23 <jhester23@ymail.com>
2020-05-13 09:14:52 -0500

    updated PantryDB.java and Sandbox.java... deleted UserBL.java and UserDAO.java

--- ProjectFolder/VeganPantry/src/com/pantry/jsp/PantryDB.java
+++ ProjectFolder/VeganPantry/src/com/pantry/jsp/PantryDB.java
@@ -11,7 +11,60 @@ public class PantryDB{
 	  
 	  
 	  
-	  
+	  public boolean authenticate(String email, String pass) throws Exception{
+		  PreparedStatement pstmt = null;
+		  String userPass = null;
+		  boolean authenticated = false;
+		  
+		  if(email == null) {
+			  System.out.println("email cannot be null");
+		  }
+		  else {
+			  try{
+				//calls getConnection method to establish connection to MySQL server
+			      
+				 
+				  Connection conn = getConnection();
+			         
+			      String selectQuery = "SELECT * From users WHERE email = '" + email + "';";
+			         
+			      if(checkExists(selectQuery, conn)) {
+			        	 pstmt = conn.prepareStatement(selectQuery);
+			        	 ResultSet rs = pstmt.executeQuery();
+				         
+			        	 if(rs.next()) {
+			        		 userPass = rs.getString("passwd");
+			        		 System.out.println(userPass);
+			        		 if(userPass == pass) {
+				        		 
+					        		authenticated = true; 
+					        	 }
+			        	 }
+			        	 
+			        	 
+
+			      }
+			  
+			         
+			      else {//executes if no user is found with the entered email address
+			        	
+			    	  System.out.println("Invalid email or password"); 
+			    	  return authenticated;
+			         }
+			         //System.out.println("addUser Executed Successfully");
+			         
+			         
+			  } catch (SQLException e) { 
+			         System.out.println("addUser Failed to Execute");
+			         e.printStackTrace();
+			         } finally {
+			           if (pstmt != null) {
+			               pstmt.close();
+			               }	          
+			           } 
+			  }
+		  return authenticated;
+	  }
 	  
 	  
 	  
@@ -33,9 +86,9 @@ public class PantryDB{
 			         //calls getConnection method to establish connection to MySQL server
 			         Connection conn = getConnection();
 			         
-			         String checkExists = "SELECT * From users WHERE email = '" + email + "';";
+			         String selectQuery = "SELECT * From users WHERE email = '" + email + "';";
 			         
-			         if(rowExists(checkExists, conn)) {
+			         if(checkExists(selectQuery, conn)) {
 			        	 
 			        	 //This executes if there is already a user with the specified email in the database.
 			        	 //There should be code here which would ask if the user forgot there username or password
@@ -44,12 +97,12 @@ public class PantryDB{
 			         
 			         else {
 			        	//this next line executes the SQL code within the quotes using connector J
-				         stmt = conn.prepareStatement("INSERT INTO Users(email)" +
-				                                                             "VALUES('" + email + "')");
+				         String updateQuery = "INSERT INTO Users(email)" + "VALUES('" + email + "')";
+			        	 stmt = conn.prepareStatement(updateQuery);
 				         stmt.executeUpdate();
 				         output = "User has been added :D";
 			         }
-			         System.out.println("addUser Executed Successfully");
+			         //System.out.println("addUser Executed Successfully");
 			         
 			         
 			       } catch (SQLException e) { 
@@ -67,7 +120,7 @@ public class PantryDB{
 	  
 	  
 	  //Checks whether a specific acrow exists in a database or not
-	  public boolean rowExists(String query, Connection conn) throws Exception{
+	  public boolean checkExists(String query, Connection conn) throws Exception{
 		  
 		  boolean exists = false;
 		  PreparedStatement pstmt = null;
@@ -82,12 +135,12 @@ public class PantryDB{
 		         pstmt = conn.prepareStatement(query);
 		         ResultSet rs = pstmt.executeQuery();
 		         if(rs.next()) {
-		        	 System.out.println("already exists");
+		        	 //System.out.println("already exists");
 		        	 exists = true;
 		        	 return exists;
 		        	 }
 		         
-		         System.out.println("rowExists Executed Successfully");
+
 		       } catch (SQLException e) { 
 		         System.out.println("rowExists Failed to Execute");
 		         e.printStackTrace();
--- ProjectFolder/VeganPantry/src/com/pantry/jsp/Sandbox.java
+++ ProjectFolder/VeganPantry/src/com/pantry/jsp/Sandbox.java
@@ -6,31 +6,28 @@ package com.pantry.jsp;
 public class Sandbox{
 
 
-public static void main(String args[]) throws Exception{
- 
- //parameters to be passed to the PantryDB object
- String dbURL = "jdbc:mysql://localhost:3306/VeganPantry";
- String dbUsername = "root";
- String dbPass = "H3ll@D@t@2020";
- 
- //creating new PantryDB object with the created parameters named connection
- PantryDB connection = new PantryDB(dbURL, dbUsername, dbPass);
- 
- //create connection with newly created PantryDB object
- //connection.getConnection();
- 
- //user email and username String
- String email = "gino@d&d.com";
- //String username = "jhester23";
- 
- 
- //add user to the VeganPantry user table
- String addUserOutput = connection.addUser(email);
- System.out.println(addUserOutput);
-}
-
-
-
-
-
+	public static void main(String args[]) throws Exception{
+		 
+		 //parameters to be passed to the PantryDB object
+		 String dbURL = "jdbc:mysql://localhost:3306/VeganPantry";
+		 String dbUsername = "root";
+		 String dbPass = "H3ll@D@t@2020";
+		 
+		 //creating new PantryDB object with the created parameters named connection
+		 PantryDB connection = new PantryDB(dbURL, dbUsername, dbPass);
+		 
+		 //create connection with newly created PantryDB object
+		 //connection.getConnection();
+		 
+		 //user email and username String
+		 String email = "gino@CC.com";
+		 String password = "HelloWorld";
+		 
+		 
+		 //test methods
+		 boolean methodOutput = connection.authenticate(email, password);
+		 System.out.println(methodOutput);
+		 
+		}
+	
 }
